ce344df17a75eaf77622d2d29406db03238ee113
Ruize Nie <rz376775@dal.ca>
2021-06-25 21:56:32 -0300

    doc: document the dart type file

--- dart_type/data_type.dart
+++ dart_type/data_type.dart
@@ -86,7 +86,7 @@ void main() {
   // Run-time -> execute the code
 
   // const & final
-  // const pi = 3.14; // lower case is prefer by Dart team
+  // const pi = 3.14; // lower case is prefer by Dart
   // pi = 3.1415926;
 
   // final name = "Ruize";
